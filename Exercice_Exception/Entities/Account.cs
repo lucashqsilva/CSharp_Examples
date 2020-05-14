8c9db4064b86d829d8993ebf982886bac421c64c
Lucas H <henrique.lluca95@gmail.com>
2020-05-14 18:35:24 -0300

    Exercicio de excecoes

---
+++ Exercice_Exception/Entities/Account.cs
@@ -0,0 +1,47 @@
+﻿using Exercice_Exception.Entities.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercice_Exception.Entities
+{
+    class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+        public double withdrawLimit { get; set; }
+
+        public Account()
+        {
+
+        }
+
+        public Account(int number, string holder, double balance, double withdrawlimit)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+            withdrawLimit = withdrawlimit;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+
+        //Aplicando as excecoes customiadas para a função de saque !
+        public void Withdraw(double amount)
+        {
+            if (amount > Balance)
+            {
+                throw new DomainException("Not enough balance !");
+            }
+            if (amount > withdrawLimit)
+            {
+                throw new DomainException("The amount exceeds Withdraw Limit !");
+            }
+            Balance -= amount;
+        }
+    }
+}
---
+++ Exercice_Exception/Program.cs
@@ -0,0 +1,42 @@
+﻿using Exercice_Exception.Entities;
+using Exercice_Exception.Entities.Exceptions;
+using System;
+using System.Globalization;
+
+namespace Exercice_Exception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter account data:");
+
+                Console.Write("Number: ");
+                int number = int.Parse(Console.ReadLine());
+
+                Console.Write("Holder Name: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Initial Balance: ");
+                double balance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Console.Write("Withdraw Limit: ");
+                double limit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Account acc = new Account(number, name, balance, limit);
+
+                Console.WriteLine();
+
+                Console.Write("Enter Amount fot Withdraw: ");
+                double w = double.Parse(Console.ReadLine());
+                acc.Withdraw(w);
+
+            }catch (DomainException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
